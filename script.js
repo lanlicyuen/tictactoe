56bfad06dbe8890aa300a330a55ebce0d545c62e
Lanlic <lanlic@hotmail.com>
2023-05-10 01:46:58 +0800

    Update script.js
    Update 1.0

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 const cells = document.querySelectorAll('.cell');
 const messageElement = document.getElementById('message');
+const xImageURL = 'path/to/your/x-image.png';
+const oImageURL = 'path/to/your/o-image.png';
 let currentPlayer = 'X';
 
 function checkWin(mark) {
-    const winCombinations = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
 
-    return winCombinations.some(combination =>
+    return winConditions.some(combination =>
         combination.every(index => cells[index].dataset.mark === mark)
     );
 }
 
-function getEmptyCells() {
-    return Array.from(cells).filter(cell => !cell.dataset.mark);
+function handleClick(event) {
+    const cell = event.target;
+
+    if (cell.dataset.mark) {
+        return;
+    }
+
+    cell.dataset.mark = currentPlayer;
+    const img = cell.querySelector('img');
+    img.src = currentPlayer === 'X' ? xImageURL : oImageURL;
+    img.alt = currentPlayer;
+    img.style.display = 'block';
+
+    if (checkWin(currentPlayer)) {
+        messageElement.textContent = `${currentPlayer} wins!`;
+        cells.forEach(cell => cell.removeEventListener('click', handleClick));
+    } else if (Array.from(cells).every(cell => cell.dataset.mark)) {
+        messageElement.textContent = "It's a draw!";
+    } else {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        if (currentPlayer === 'O') {
+            computerMove();
+            if (checkWin(currentPlayer)) {
+                messageElement.textContent = `${currentPlayer} wins!`;
+                cells.forEach(cell => cell.removeEventListener('click', handleClick));
+            } else {
+                currentPlayer = 'X';
+            }
+        }
+    }
 }
 
 function computerMove() {
@@ -31,64 +66,6 @@ function computerMove() {
     }
 }
 
-
-function handleClick(event) {
-    const cell = event.target;
-
-    if (cell.dataset.mark) {
-        return;
-    }
-
-    cell.dataset.mark = currentPlayer;
-    cell.textContent = currentPlayer;
-
-    if (checkWin(currentPlayer)) {
-        messageElement.textContent = `玩家 ${currentPlayer} 胜出咯!`;
-        cells.forEach(cell => cell.removeEventListener('click', handleClick));
-        return;
-    } else if (getEmptyCells().length === 0) {
-        messageElement.textContent = '平手咯!';
-        return;
-    }
-
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    computerMove();
-
-    if (checkWin(currentPlayer)) {
-        messageElement.textContent = `玩家 ${currentPlayer} 输掉咯!`;
-        cells.forEach(cell => cell.removeEventListener('click', handleClick));
-        return;
-    } else if (getEmptyCells().length === 0) {
-        messageElement.textContent = '平手咯!';
-        return;
-    }
-
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-}
-
-cells.forEach(cell => cell.addEventListener('click', handleClick));
-
-// Add the event listener for the reset button
-const resetButton = document.getElementById('reset');
-
-resetButton.addEventListener('click', () => {
-    location.reload();
-});
-function minimax(board, depth, isMaximizing) {
-    // ...
-}
-
-function getBestMove(board) {
-    // ...
-}
-
-function isBoardFull(board) {
-    // ...
-}
-
-function checkWinOnBoard(board) {
-    // ...
-}
 function minimax(board, depth, isMaximizing) {
     let winner = checkWinOnBoard(board);
     if (winner !== null) {
@@ -121,9 +98,7 @@ function minimax(board, depth, isMaximizing) {
             }
         }
         return bestScore;
-    }
-}
-
+   
 function getBestMove(board) {
     let bestScore = -Infinity;
     let move;
@@ -144,12 +119,35 @@ function getBestMove(board) {
     return move;
 }
 
-function computerMove() {
-    const board = Array.from(cells).map(cell => cell.dataset.mark);
-    const move = getBestMove(board);
-
-    if (move !== undefined) {
-        cells[move].dataset.mark = currentPlayer;
-        cells[move].textContent = currentPlayer;
-    }
+function isBoardFull(board) {
+    return board.every(cell => cell !== null);
 }
+
+function checkWinOnBoard(board) {
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let condition of winConditions) {
+        const [a, b, c] = condition;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return board[a];
+        }
+    }
+
+    return null;
+}
+
+// Add the event listener for the reset button
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', () => {
+    location.reload();
+});
