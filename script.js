9c5282fe0b1365df61dd1bf09475c1e9bbd46f60
Lanlic <lanlic@hotmail.com>
2023-05-11 01:06:16 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,153 +1,140 @@
 const cells = document.querySelectorAll('.cell');
-const messageElement = document.getElementById('message');
-const xImageURL = 'path/to/your/x-image.png';
-const oImageURL = 'path/to/your/o-image.png';
+const winnerMessage = document.getElementById('winner-message');
+const resetButton = document.getElementById('reset-button');
+let board = Array(9).fill(null);
 let currentPlayer = 'X';
+let winner = null;
 
-function checkWin(mark) {
-    const winConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+cells.forEach((cell, index) => {
+  cell.addEventListener('click', () => handleClick(index));
+});
 
-    return winConditions.some(combination =>
-        combination.every(index => cells[index].dataset.mark === mark)
-    );
+resetButton.addEventListener('click', resetGame);
+
+function renderBoard() {
+  cells.forEach((cell, index) => {
+    cell.textContent = board[index];
+  });
 }
 
-function handleClick(event) {
-    const cell = event.target;
+function handleClick(index) {
+  if (board[index] === null && winner === null) {
+    board[index] = currentPlayer;
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    winner = checkWinner(board);
+    renderBoard();
+    displayWinner();
 
-    if (cell.dataset.mark) {
-        return;
+    if (!winner && !isBoardFull(board)) {
+      const bestMove = findBestMove(board);
+      board[bestMove] = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      winner = checkWinner(board);
+      renderBoard();
+      displayWinner();
     }
-
-    cell.dataset.mark = currentPlayer;
-    const img = cell.querySelector('img');
-    img.src = currentPlayer === 'X' ? xImageURL : oImageURL;
-    img.alt = currentPlayer;
-    img.style.display = 'block';
-
-    if (checkWin(currentPlayer)) {
-        messageElement.textContent = `${currentPlayer} wins!`;
-        cells.forEach(cell => cell.removeEventListener('click', handleClick));
-    } else if (Array.from(cells).every(cell => cell.dataset.mark)) {
-        messageElement.textContent = "It's a draw!";
-    } else {
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        if (currentPlayer === 'O') {
-            computerMove();
-            if (checkWin(currentPlayer)) {
-                messageElement.textContent = `${currentPlayer} wins!`;
-                cells.forEach(cell => cell.removeEventListener('click', handleClick));
-            } else {
-                currentPlayer = 'X';
-            }
-        }
-    }
+  }
 }
 
-function computerMove() {
-    const board = Array.from(cells).map(cell => cell.dataset.mark);
-    const move = getBestMove(board);
+function checkWinner(board) {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-    if (move !== undefined) {
-        cells[move].dataset.mark = currentPlayer;
-        const img = cells[move].querySelector('img');
-        img.src = currentPlayer === 'X' ? xImageURL : oImageURL;
-        img.alt = currentPlayer;
-        img.style.display = 'block';
+  for (const line of winningLines) {
+    const [a, b, c] = line;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
     }
+  }
+
+  return null;
+}
+
+function displayWinner() {
+  if (winner === 'X') {
+    winnerMessage.textContent = '恭喜你胜出!';
+  } else if (winner === 'O') {
+    winnerMessage.textContent = '你已败北！';
+  } else if (isBoardFull(board)) {
+    winnerMessage.textContent = '可惜！居然达成平手。';
+  }
+}
+
+
+function isBoardFull(board) {
+  return board.every(cell => cell !== null);
+}
+
+function findBestMove(board) {
+  let bestMove = -1;
+  let bestScore = -Infinity;
+
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === null) {
+      board[i] = 'O';
+      const score = minimax(board, 5, false);
+      board[i] = null;
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return bestMove;
 }
 
 function minimax(board, depth, isMaximizing) {
-    let winner = checkWinOnBoard(board);
-    if (winner !== null) {
-        return winner === 'X' ? -1 : 1;
-    }
+  const winner = checkWinner(board);
 
-    if (depth === 0 || isBoardFull(board)) {
-        return 0;
-    }
+  if (winner !== null) {
+    return winner === 'X' ? -10 : 10;
+  }
 
-    if (isMaximizing) {
-        let bestScore = -Infinity;
-        for (let i = 0; i < board.length; i++) {
-            if (!board[i]) {
-                board[i] = 'O';
-                let score = minimax(board, depth - 1, false);
-                board[i] = null;
-                bestScore = Math.max(score, bestScore);
-            }
-        }
-        return bestScore;
-    } else {
-        let bestScore = Infinity;
-        for (let i = 0; i < board.length; i++) {
-            if (!board[i]) {
-                board[i] = 'X';
-                let score = minimax(board, depth - 1, true);
-                board[i] = null;
-                bestScore = Math.min(score, bestScore);
-            }
-        }
-        return bestScore;
-   
-function getBestMove(board) {
+  if (depth === 0 || isBoardFull(board)) {
+    return 0;
+  }
+
+  if (isMaximizing) {
     let bestScore = -Infinity;
-    let move;
-
     for (let i = 0; i < board.length; i++) {
-        if (!board[i]) {
-            board[i] = 'O';
-            let score = minimax(board, 3, false);
-            board[i] = null;
-
-            if (score > bestScore) {
-                bestScore = score;
-                move = i;
-            }
-        }
+      if (board[i] === null) {
+        board[i] = 'O';
+        const score = minimax(board, depth - 1, false);
+        board[i] = null;
+        bestScore = Math.max(score, bestScore);
+      }
     }
-
-    return move;
-}
-
-function isBoardFull(board) {
-    return board.every(cell => cell !== null);
-}
-
-function checkWinOnBoard(board) {
-    const winConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    for (let condition of winConditions) {
-        const [a, b, c] = condition;
-        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            return board[a];
-        }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < board.length; i++) {
+    if (board[i] === null) {
+    board[i] = 'X';
+    const score = minimax(board, depth - 1, true);
+    board[i] = null;
+    bestScore = Math.min(score, bestScore);
     }
-
-    return null;
-}
-
-// Add the event listener for the reset button
-const resetButton = document.getElementById('reset');
-
-resetButton.addEventListener('click', () => {
-    location.reload();
-});
+    }
+    return bestScore;
+    }
+    }
+    
+    function resetGame() {
+    board = Array(9).fill(null);
+    currentPlayer = 'X';
+    winner = null;
+    winnerMessage.textContent = '';
+    renderBoard();
+    }
+    
+    renderBoard();
